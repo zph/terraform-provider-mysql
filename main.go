9a8ad154b50937bb5d2eb5fe49746a01e73e6aea
Zander Hill <zander@xargs.io>
2024-07-05 19:35:46 -0700

    Iterate on publishing docs with each release

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/petoju/terraform-provider-mysql/v3/mysql"
+  // Added to ensure we have tfplugindocs available in go.mod
+  _ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
 
 func main() {
