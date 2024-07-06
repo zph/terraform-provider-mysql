08a697a33b2646256a217ec988aed5a338be9f10
Zander Hill <zander@xargs.io>
2024-07-05 19:39:19 -0700

    Try again with doc creation

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/petoju/terraform-provider-mysql/v3/mysql"
   // Added to ensure we have tfplugindocs available in go.mod
-  _ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+  _ "github.com/hashicorp/terraform-plugin-docs"
 )
 
 func main() {
