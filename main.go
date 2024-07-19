95340654656d243c58c1a13a5629d6d9dc266ac2
Zander Hill <zander@xargs.io>
2024-07-18 23:24:35 -0700

    Fix go mod tidy and disable doc building (#4)

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/petoju/terraform-provider-mysql/v3/mysql"
